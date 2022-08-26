9ebaa05f97c07807a6f00a6dce15ab815c16687f
dsaragih <dabestender@gmail.com>
2022-08-26 17:42:51 -0400

    Added board creation mechanics and other small features

--- game.py
+++ game.py
@@ -5,11 +5,15 @@ try:
     import sys
     import time
     import numpy as np
-    from typing import Tuple
+    from typing import Tuple, List
     from pygame.locals import *
 except ImportError as err:
-    print ("couldn't load module. %s" % (err))
+    print("couldn't load module. %s" % (err))
     sys.exit(2)
+
+"""
+TODO: Board creation, Backtracking animation
+"""
 
 
 class Grid:
@@ -18,42 +22,72 @@ class Grid:
     height: int
     square_width: int
     matrix: np.array
+    screen: pygame.display
+    is_solved: bool
+    to_be_highlighted: List[Tuple[int, int]]
     selected_square: Tuple[int, int]
 
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self, width: int, height: int, screen: pygame.display) -> None:
         self.width = width
         self.height = height
         self.square_width = width // 9
         self.matrix = np.zeros((9, 9), dtype=np.intp)
+        self.screen = screen
         self.selected_square = None
+        self.to_be_highlighted = None
+        self.create_game()
+    
+    def create_game(self) -> None:
+        self.solve()
+        for _ in range(3):
+            # Swaps rows
+            r1, r2 = random.sample(range(3), 2)
+            self.matrix[[r1, r2]] = self.matrix[[r2, r1]]
+            self.matrix[[r1 + 3, r2 + 3]] = self.matrix[[r2 + 3, r1 + 3]]
+            self.matrix[[r1 + 6, r2 + 6]] = self.matrix[[r2 + 6, r1 + 6]]
 
-    def _row_safe(self, row: int, n: int) -> bool:
+        for _ in range(3):
+            # Swaps columns
+            c1, c2 = random.sample(range(3), 2)
+            self.matrix[[c1, c2]] = self.matrix[[c2, c1]]
+            self.matrix[[c1 + 3, c2 + 3]] = self.matrix[[c2 + 3, c1 + 3]]
+            self.matrix[[c1 + 6, c2 + 6]] = self.matrix[[c2 + 6, c1 + 6]]
+
+        for _ in range(30):
+            x, y = random.choice(range(-4, 5)), random.choice(range(-4, 5))
+            self.matrix[x + 4, y + 4] = 0
+            self.matrix[4 - x, 4 - y] = 0
+
+
+    def _find_row_dupes(self, row: int, n: int) -> Tuple[int, int]:
         for i in range(9):
             if self.matrix[row, i] == n:
-                return False
-        return True
+                return (row, i)
 
-    def _col_safe(self, col: int, n: int) -> bool:
+    def _find_col_dupes(self, col: int, n: int) -> Tuple[int, int]:
         for i in range(9):
             if self.matrix[i, col] == n:
-                return False
-        return True
+                return (i, col)
 
-    def _sub_grid_safe(self, row: int, col: int, n: int) -> bool:
+    def _find_sub_grid_dupes(self, row: int, col: int, n: int) -> Tuple[int, int]:
         start_row = row - row % 3
         start_col = col - col % 3
         for i in range(3):
             for j in range(3):
                 if self.matrix[start_row + i, start_col + j] == n:
-                    return False
-        return True
-    
+                    return (start_row + i, start_col + j)
+
     def _input_safe(self, row: int, col: int, n: int) -> bool:
         """Checks whether {n} is a valid number to be placed in
         self.matrix[row, col].
         """
-        return self._row_safe(row, n) and self._col_safe(col, n) and \
-            self._sub_grid_safe(row, col, n)
+        return not(self._find_row_dupes(row, n) or self._find_col_dupes(col, n) or \
+            self._find_sub_grid_dupes(row, col, n))
+    
+    def _get_dupes(self, row: int, col: int, n: int) -> List[Tuple[int, int]]:
+        res = [self._find_row_dupes(row, n), self._find_col_dupes(col, n), \
+            self._find_sub_grid_dupes(row, col, n)]
+        return list(filter(lambda x: x is not None, res))
 
     def _find_empty(self) -> Tuple[int, int]:
         """Finds the next empty square"""
@@ -61,15 +95,18 @@ class Grid:
             for j in range(9):
                 if self.matrix[i, j] == 0:
                     return (i, j)
-    
+
     def clear(self) -> None:
         """Clears the grid."""
         self.matrix = np.zeros((9, 9), dtype=np.intp)
+        self.create_game()
 
     def solve(self) -> bool:
         """Backtracks to find the solution to the Sudoku."""
         empty_square = self._find_empty()
         if not empty_square:
+            # Solved
+            self.is_solved = True
             return True
 
         row = empty_square[0]
@@ -89,17 +126,29 @@ class Grid:
         y = math.floor(pos[1] / self.square_width) * self.square_width
         self.selected_square = (x, y)
 
-    def highlight_square(self, screen: pygame.display) -> None:
+    def highlight_square(self) -> None:
         """Fills in the selected square with a light blue color."""
-        if not self.selected_square: return
+        if not self.selected_square:
+            return
 
         x, y = self.selected_square
-        background = pygame.Surface((self.square_width - 2, self.square_width - 2))
+        background = pygame.Surface(
+            (self.square_width - 2, self.square_width - 2))
         background.convert()
         background.fill((204, 255, 255))
-        
+
         # Small adjustment to ensure the fill fits inside the square
-        screen.blit(background, (x + 1, y + 1))
+        self.screen.blit(background, (x + 1, y + 1))
+    
+    def highlight_dupes(self) -> None:
+        fade = pygame.Surface((self.square_width - 2, self.square_width - 2))
+        fade.convert()
+        fade.fill((255, 114, 111))
+        
+        for alpha in range(300, 0, -1):
+            fade.set_alpha(60)
+            for (row, col) in self.to_be_highlighted:
+                self.screen.blit(fade, (col * self.square_width + 1, row * self.square_width + 1))
 
     def update_selected_square(self, input: int) -> None:
         """Updates the number on the selected square."""
@@ -107,38 +156,45 @@ class Grid:
         row //= self.square_width
         col //= self.square_width
 
-        if self._input_safe(row, col, input): self.matrix[row, col] = input
+        is_safe = self._input_safe(row, col, input)
+        if is_safe and not self.is_solved:
+            self.matrix[row, col] = input
+        elif not is_safe:
+            self.to_be_highlighted = self._get_dupes(row, col, input)
+        
+        self.is_solved = 0 not in self.matrix
 
-
-    def draw(self, screen: pygame.display) -> None:
+    def draw(self) -> None:
         """Draws everything on the grid."""
-
         for div in range(3 * self.square_width, self.width, 3 * self.square_width):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(div, 0, 2, self.height))
+            pygame.draw.rect(self.screen, (0, 0, 0),
+                             pygame.Rect(div, 0, 2, self.height))
         for div in range(3 * self.square_width, self.height, 3 * self.square_width):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, div, self.width, 2))
+            pygame.draw.rect(self.screen, (0, 0, 0),
+                             pygame.Rect(0, div, self.width, 2))
         for div in range(self.square_width, self.width, self.square_width):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(div, 0, 1, self.height))
+            pygame.draw.rect(self.screen, (0, 0, 0),
+                             pygame.Rect(div, 0, 1, self.height))
         for div in range(self.square_width, self.height, self.square_width):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, div, self.width, 1))
+            pygame.draw.rect(self.screen, (0, 0, 0),
+                             pygame.Rect(0, div, self.width, 1))
 
-        
-        self.highlight_square(screen)
-        
-        # Scoring mechanism to be added.
+        self.highlight_square()
+        if self.to_be_highlighted: self.highlight_dupes()
+
         NUMBER_FONT = pygame.font.SysFont("Arial", 30)
-        SCORE_FONT = pygame.font.SysFont("Arial", 100)
 
         # Displays the integers on self.matrix
         for y in range(9):
             for x in range(9):
                 if self.matrix[y, x] != 0:
-                    number_text = NUMBER_FONT.render(str(self.matrix[y, x]), 1, (0, 0, 0))
-                    font_x = (self.square_width - number_text.get_width())//2 + self.square_width*x
-                    font_y = (self.square_width - number_text.get_height())//2 + self.square_width*y
-                    screen.blit(number_text, (font_x, font_y))
-        
-
+                    number_text = NUMBER_FONT.render(
+                        str(self.matrix[y, x]), 1, (0, 0, 0))
+                    font_x = (self.square_width -
+                              number_text.get_width())//2 + self.square_width*x
+                    font_y = (self.square_width -
+                              number_text.get_height())//2 + self.square_width*y
+                    self.screen.blit(number_text, (font_x, font_y))
 
 def main() -> None:
     # Initialize the screen
@@ -152,11 +208,11 @@ def main() -> None:
     background.fill((250, 250, 250))
 
     # Initialize the grid
-    grid = Grid(640, 640)
+    grid = Grid(640, 640, screen)
 
     # Blit everything to the screen
     screen.blit(background, (0, 0))
-    grid.draw(screen)
+    grid.draw()
     pygame.display.flip()
 
     clock = pygame.time.Clock()
@@ -183,9 +239,10 @@ def main() -> None:
                     grid.clear()
 
         screen.blit(background, (0, 0))
-        grid.draw(screen)
+        grid.draw()
         pygame.display.flip()
+        pygame.time.wait(100)
 
 
-
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
